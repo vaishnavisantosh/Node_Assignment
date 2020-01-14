ff51376d06a86a60c00829f711944369a6555462
vaishnavisantosh <vaishnavijawanjal29@gmail.com>
2020-01-14 20:27:36 +0530

    working on login and signup

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express')
 var router = express.Router()
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource')
+router.get('/login', (req, res, next) => {
+  res.send('its an login page')
+})
+
+router.get('/signup', (req, res, next) => {
+  res.send('its an signup page')
 })
 
 module.exports = router
